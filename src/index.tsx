ca066531c1dab66789c339570f568750c6dc2ebd
saalik123 <63722568+saalik123@users.noreply.github.com>
2022-02-07 19:35:53 +0530

    removed reavt-dnd

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { DndProvider } from "react-dnd";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
